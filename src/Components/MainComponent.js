d5ca746421488dc0e3d449a0106b534975163c3f
Daniel Ibarrola <daniel.ibarrola.sanchez@gmail.com>
2020-12-01 21:16:14 -0600

    Configured React

---
+++ src/Components/MainComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './HeaderComponent'
+
+function MainComponent() {
+    return (
+        <div className="main-component">
+            <Header />
+        </div>
+    )
+}
+
+export default MainComponent
